fee5fc943c1bb15fdfa0074f61ac9cb5b01b6f81
Haike <linkstar0513@gmail.com>
2019-11-16 14:54:39 +0800

    commit

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { BrowserRouter as Router, Switch, Link, Route, useHistory, useRouteMatch } from 'react-router-dom'
 import './index.scss'
 import Menu, { MenuProps } from '../Components/Menu'
 // 也可以使用interface
@@ -11,16 +12,34 @@ type LayoutProps = {
 const Layout: React.FC<LayoutProps> = (props: LayoutProps) => {
 
   const store = useSelector(state => state);
+  // const history = useHistory()
   console.log("中心store", store)
+  // console.log("history", history)
+
 
     return (
       <div className={"layout-container"}>
         <div className={"layout-left"}>
-          {props.menus.map((menu, index) => {
-            return <Menu {...menu} />
-          })}
+          <Router>
+            {props.menus.map((menu, index) => {
+              return <Link to={menu.url}><Menu {...menu} /></Link>
+              
+            })}
+          </Router>
         </div>
         <div className={"layout-content"}>
+          <Router>
+            <Switch>
+              <Route path={"/start"}>
+              </Route>
+              <Route path={"/start"}>
+              </Route>
+              <Route path={"/start"}>
+              </Route>
+              <Route path={"/start"}>
+              </Route>
+            </Switch>
+          </Router>
           {props.children}   
         </div>
       </div>
