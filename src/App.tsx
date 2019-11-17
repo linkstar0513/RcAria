a7b817aa8d822aedd1858e0785bd61fcc363f28f
Haike <linkstar0513@gmail.com>
2019-11-17 19:14:33 +0800

    api

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import {FetchGet} from "./utils/Fetch"
+import Aria2 from "Aria2"
+import ContainerFooter from "Components/ContainerFooter"
+import { Container, Row, Col} from 'react-bootstrap'
 import logo from './logo.svg';
 import './App.css';
+
 
 import Layout from './Layout'
 import menus from './config/menus'
 
 const App: React.FC = () => {
-  const rs = FetchGet("http://www.baidu.com").then(rs => {
-    console.log(rs)
-  });
+  // const rs = FetchGet("http://www.baidu.com").then(rs => {
+  //   console.log(rs)
+  // });
+  const [speedInfo, setSpeedInfo] = useState<any>({})
 
   return (
     <Layout menus={menus}>
       <div>layout</div>
+      <ContainerFooter />
     </Layout>
   );
 }
